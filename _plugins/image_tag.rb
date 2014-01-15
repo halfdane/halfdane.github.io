312e96d1cdc0ae543548c8e36dfa81412b790c0b
halfdane <halfdane@gmx.net>
2014-01-15 11:03:11 +0100

    Lazyload images

--- _plugins/image_tag.rb
+++ _plugins/image_tag.rb
@@ -12,6 +12,7 @@ module Jekyll
     @url = nil
     @title = nil
     @css = nil
+    @markup = nil
 
     IMAGE_URL_WITH_TITLE_AND_CSS = /(\S+)(\s+)"(.*?)"(\s+)"(.*?)"/i
     IMAGE_URL_WITH_TITLE = /(\S+)(\s+)"(.*?)"/i
@@ -32,6 +33,8 @@ module Jekyll
         @title = ""
       end
 
+      @markup = markup
+
       @css += " " + givenCss unless givenCss.nil?
 
       # Config options
@@ -41,49 +44,19 @@ module Jekyll
       @config['retina_suffix']    ||= '@2x'
       @config['lazy']             ||= false
       @config['lazy_placeholder'] ||= ''
-
-      # Build url
-      # Only works if no trailing slash in `root_url`, and no leading in `url`
-      @url = [@config['root_url'], @url].join('/') unless (@url=~/^http/)
-      @lazy_placeholder_url = [@config['root_url'], @config['lazy_placeholder']].join('/')
     end
 
     def render(context)
+
+      imageUrl = Liquid::Template.parse(@url).render context
+
+      # Build url
+      # Only works if no trailing slash in `root_url`, and no leading in `url`
+      imageUrl = [@config['root_url'], imageUrl].join('/') unless (imageUrl=~/^http/)
+
       source = "<figure class=\"#{@css}\">"
 
-      # Retina
-      if @config['retina']
-
-
-        if @config['lazy']
-          # Retina and lazy
-          source += "<span data-picture data-alt=\"#{@title}\" data-class=\"lazy\">"
-
-          source += "<span data-src-lazy=\"#{@url}\" data-src=\"#{@lazy_placeholder_url}\"></span>"
-          source += "<span data-src-lazy=\"#{retina_url(@url)}\" data-src=\"#{@lazy_placeholder_url}\" "
-          source += "data-media=\"(min-resolution: 192dpi),(-webkit-min-device-pixel-ratio: 2),(min--moz-device-pixel-ratio: 2),(-o-min-device-pixel-ratio: 2/1),(min-device-pixel-ratio: 2),(min-resolution: 2dppx)\">"
-          source += "</span>"
-        else
-          # Retina only
-          source += "<span data-picture data-alt=\"#{@title}\">"
-
-          source += "<span data-src=\"#{@url}\"></span>"
-          source += "<span data-src=\"#{retina_url(@url)}\" "
-          source += "data-media=\"(min-resolution: 192dpi),(-webkit-min-device-pixel-ratio: 2),(min--moz-device-pixel-ratio: 2),(-o-min-device-pixel-ratio: 2/1),(min-device-pixel-ratio: 2),(min-resolution: 2dppx)\">"
-          source += "</span>"
-        end
-
-        source += "<noscript><img src=\"#{@url}\" alt=\"#{@title}\"></noscript>"
-        source += "</span>"
-
-        # Lazy only
-      elsif @config['lazy']
-        source += "<img class=\"lazy\" data-src=\"#{@url}\" src=\"#{@lazy_placeholder_url}\">"
-
-        # Classic
-      else
-        source += "<img src=\"#{@url}\" alt=\"#{@title}\">"
-      end
+      source += "<img src=\"/assets/img/loader.gif\" data-src=\"#{imageUrl}\" alt=\"#{@title}\">"
 
       site = context.registers[:site]
       converter = site.getConverterImpl(::Jekyll::Converters::Markdown)
