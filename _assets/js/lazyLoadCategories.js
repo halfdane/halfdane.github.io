74db22b3a956acac15fd6b70c7df448b7b3deeb1
halfdane <halfdane@gmx.net>
2014-02-10 13:34:34 +0100

    Diese abgerundeten Kanten kann doch kein Mensch mehr sehen.

--- _assets/js/lazyLoadCategories.js
+++ _assets/js/lazyLoadCategories.js
@@ -3,27 +3,8 @@ var halfdane = halfdane || {};
 halfdane.lazyloadCategories = function (categories_target_selector, remaining_contents_selector) {
     'use strict';
 
-    // Get the original CSS values instead of values of the element.
-    // @param {String} ruleSelector
-    // @param {String} cssprop
-    // @returns {String} property of the style
-    function getCssStyle(ruleSelector, cssprop) {
-        for (var c = 0, lenC = document.styleSheets.length; c < lenC; c++) {
-            var rules = document.styleSheets[c].cssRules;
-            for (var r = 0, lenR = rules.length; r < lenR; r++) {
-                var rule = rules[r];
-                if (rule.selectorText === ruleSelector && rule.style) {
-                    return rule.style[cssprop]; // rule.cssText;
-                }
-            }
-        }
-        return null;
-    }
-
     var $categories_target = $(categories_target_selector);
     if ($categories_target.is(":visible")) {
-        var targetWidth = getCssStyle(categories_target_selector, "width");
-        targetWidth = targetWidth.replace(/%/, "");
 
         $.getJSON("json/categories.json", function (categoriesJson) {
             var $categories = $('<ul></ul>').appendTo($categories_target);
@@ -34,11 +15,11 @@ halfdane.lazyloadCategories = function (categories_target_selector, remaining_co
 
             $(categoriesJson.categories).each(function (index, currentCategory) {
                 var $category = $('<li></li>')
-                    .addClass('tile')
                     .append(
                         $('<a></a>').attr('href', currentCategory.url).text(currentCategory.title)
                     )
                     .appendTo($categories);
+
                 var $posts = $('<ul></ul>').appendTo($category);
                 $(currentCategory.posts).each(function (index, currentPost) {
                     var $post = $('<li></li>')
@@ -48,13 +29,6 @@ halfdane.lazyloadCategories = function (categories_target_selector, remaining_co
                         .appendTo($posts);
                 });
             });
-
-            $(remaining_contents_selector)
-                .css({display: "inline-block"})
-                .animate({width: 99 - targetWidth + "%"}, { duration: 200, queue: false });
-            $categories_target
-                .css({display: "inline-block", width: 0})
-                .animate({width: targetWidth + "%"}, { duration: 200, queue: false });
         });
     }
 };
