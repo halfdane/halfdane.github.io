c4ec90a370754d39f3d3de631d2bf9a970c7619a
halfdane <halfdane@gmx.net>
2014-10-23 22:00:31 +0200

    dont encode apersand

--- assets/js/solargraph/createMail.js
+++ assets/js/solargraph/createMail.js
@@ -4,7 +4,7 @@ $(function() {
 	var content='Mail an halfdane@gmx.net wegen Dose '+dose;
 	var subject='Solargraph-Dose '+dose+' gefunden';
 	var body='Hallo Halfdane,\nich habe die Dose '+dose+' gefunden.\n\nMit freundlichen Grüßen '
-	var mailto='mailto:halfdane@gmx.net?subject='+encodeURIComponent(subject)+'&amp;body'+encodeURIComponent(body);
+	var mailto='mailto:halfdane@gmx.net?subject='+encodeURIComponent(subject)+'&body'+encodeURIComponent(body);
 	$('pre').replaceWith($('<a>').attr('href',mailto).text(content));
   }
 });
