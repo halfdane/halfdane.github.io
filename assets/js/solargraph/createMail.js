c771cb9d631ec52c54c0f5aea692345ce6b842a3
halfdane <halfdane@gmx.net>
2014-10-23 19:44:16 +0000

    [ci skip] snapshot 10/23/14 19:44

--- assets/js/solargraph/createMail.js
+++ assets/js/solargraph/createMail.js
@@ -4,7 +4,7 @@ $(function() {
 	var content='Mail an halfdane@gmx.net wegen Dose '+dose;
 	var subject='Solargraph-Dose '+dose+' gefunden';
 	var body='Hallo Halfdane,\nich habe die Dose '+dose+' gefunden.\n\nMit freundlichen Grüßen '
-	var mailto='mailto:halfdane@gmx.net?'+subject+'&'+body;
-	$('pre').replaceWith($('<a>').attr('href',encodeURI(mailto)).text(content));
+	var mailto='mailto:halfdane@gmx.net?subject='+encodeURI(subject)+'&body'+encodeURI(body);
+	$('pre').replaceWith($('<a>').attr('href',mailto).text(content));
   }
 });
