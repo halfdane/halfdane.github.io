dbaad6b1f3649af751865adc1d3ae794666437bf
halfdane <halfdane@gmx.net>
2014-11-28 09:41:06 +0100

    lazy loading finally works

--- contents/js/lazy_loading.js
+++ contents/js/lazy_loading.js
@@ -7,31 +7,26 @@ halfdane.lazyload = (function () {
     function loadNext(element){
         console.log('LOADING');
 
-        var sexhr = new window.sexhr();
-        sexhr.req({
-            url: $('#next')[0].getAttribute('href'),
-            done: function(err, res) {
-                console.log("Done");
-                if (err) throw err;
-                var tempDiv = document.createElement('div');
-                tempDiv.innerHTML = res.text;
+        microAjax($('#next')[0].getAttribute('href'), function (res) {
+            console.log("Done");
+            var tempDiv = document.createElement('div');
+            tempDiv.innerHTML = res;
 
-                var node = $('#articles')[0];
-                var newArticles = $(tempDiv).find('.article');
-                $(newArticles).forEach(function(el){
-                    node.appendChild(el);
-                });
+            var node = $('#articles')[0];
+            var newArticles = $(tempDiv).find('.article');
+            $(newArticles).forEach(function(el){
+                node.appendChild(el);
+            });
 
-                var nextNext = $(tempDiv).findOne('#next').getAttribute('href');
-                $('#next')[0].setAttribute('href', nextNext);
+            var nextNext = $(tempDiv).findOne('#next').getAttribute('href');
+            $('#next')[0].setAttribute('href', nextNext);
 
-                console.log("registering sloth for: ", $('#next')[0]);
-                sloth({
-                    on: $('#next')[0],
-                    threshold: 100,
-                    callback: loadNext
-                });
-            }
+            console.log("registering sloth for: ", $('#next')[0]);
+            sloth({
+                on: $('#next')[0],
+                threshold: 100,
+                callback: loadNext
+            });
         });
     }
 
