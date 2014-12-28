b4396705412ccb00c0143f7b87085a89d7525b68
halfdane <halfdane@gmx.net>
2014-12-28 09:45:32 +0000

    [ci skip] snapshot 12/28/14 09:45

---
+++ solargraph/createMail.js
@@ -0,0 +1,10 @@
+$(function() {
+  var dose=window.location.hash;
+  if (dose) {
+	var content='Mail an halfdane@gmx.net wegen der Dose '+dose;
+	var subject='Solargraph-Dose '+dose+' gefunden';
+	var body='Hallo Halfdane,\nich habe die Dose '+dose+' gefunden.\n\nMit freundlichen Grüßen '
+	var mailto='mailto:halfdane@gmx.net?subject='+encodeURIComponent(subject)+'&body'+encodeURIComponent(body);
+	$('.noJs').replaceWith($('<a>').attr('href',mailto).text(content));
+  }
+});
