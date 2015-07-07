e3d1682b9744d9f4377786102dd82314f7f31c51
halfdane <halfdane@gmx.net>
2015-07-07 10:42:47 +0000

    [ci skip] snapshot 07/07/15 10:42

--- articles/2015-06-27-picture-swipe/jTinderSwipe.js
+++ articles/2015-06-27-picture-swipe/jTinderSwipe.js
@@ -1,3 +1,4 @@
+/*global $ */
 var halfdane = halfdane || {};
 
 halfdane.fake_model = (function () {
@@ -42,197 +43,85 @@ halfdane.fake_model = (function () {
     };
 }());
 
-halfdane.dragger = function (drag, drop) {
-    'use strict';
-
-    var touchStart = false;
-    var xStart = 0, yStart = 0;
-    var pageX = 0, pageY = 0;
-
-    function handler(ev) {
-        ev.preventDefault();
-
-        switch (ev.type) {
-            case 'touchstart':
-                if (touchStart === false) {
-                    touchStart = true;
-                    xStart = ev.touches[0].pageX;
-                    yStart = ev.touches[0].pageY;
-                }
-                break;
-            case 'mousedown':
-                if (touchStart === false) {
-                    touchStart = true;
-                    xStart = ev.pageX;
-                    yStart = ev.pageY;
-                }
-                break;
-            case 'mousemove':
-            case 'touchmove':
-                if (touchStart === true) {
-                    pageX = typeof ev.touches === 'undefined' ? ev.pageX : ev.touches[0].pageX;
-                    pageY = typeof ev.touches === 'undefined' ? ev.pageY : ev.touches[0].pageY;
-
-                    drag(parseInt(pageX) - parseInt(xStart), parseInt(pageY) - parseInt(yStart));
-                }
-                break;
-            case 'mouseup':
-            case 'touchend':
-                touchStart = false;
-                pageX = (typeof ev.changedTouches === 'undefined') ? ev.pageX : ev.changedTouches[0].pageX;
-                pageY = (typeof ev.changedTouches === 'undefined') ? ev.pageY : ev.changedTouches[0].pageY;
-
-                drop(parseInt(pageX) - parseInt(xStart), parseInt(pageY) - parseInt(yStart));
-                break;
-        }
-    }
-
-    function bind(element) {
-        element.addEventListener('touchstart', handler);
-        element.addEventListener('mousedown', handler);
-        element.addEventListener('touchmove', handler);
-        element.addEventListener('mousemove', handler);
-        element.addEventListener('touchend', handler);
-        element.addEventListener('mouseup', handler);
-    }
-
-    return {
-        bind: bind
-    };
-};
-
 halfdane.pictureswipe = function (model) {
     'use strict';
-    
-    var imagesContainer = document.getElementById('images');
-    var love = document.getElementById('love');
-    var hate = document.getElementById('hate');
+
+    var $imagesContainer = $('#images');
+    var imagesContainer = $imagesContainer[0];
+    var $love = $('#love');
+    var $hate = $('#hate');
+    var love = $love[0];
+    var hate = $hate[0];
+
+    var jTinder = null;
 
     function currentTopImage() {
         return imagesContainer.lastElementChild;
     }
 
-    function deleteElement(isHot) {
-        var element = currentTopImage();
-        var articleNumber = element.getAttribute('data-articleNumber');
+    function deleteElement(isHot, $element) {
+        var articleNumber = $element.attr('data-articleNumber');
         model.setDecision(articleNumber, isHot);
 
-        element.addEventListener("transitionend", function transitionEndListener(e) {
-            e.target.removeEventListener(e.type, transitionEndListener);
+        $element.remove();
+        love.classList.remove('active');
+        hate.classList.remove('active');
 
-            imagesContainer.removeChild(element);
-            love.classList.remove('active');
-            hate.classList.remove('active');
-
-            initTouchListeners();
-
-            if (imagesContainer.childElementCount < 5) {
-                loadSomeImages();
-            }
-        });
-
-        window.requestAnimationFrame(function () {
-            var imageThrowTarget = window.innerWidth * (isHot ? 1 : -1);
-            element.style.transition = "0.2s";
-            element.style.transform = "translate(" + imageThrowTarget + "px, " + 0 + "px) rotate(" +
-            (imageThrowTarget / 10) + "deg)";
-            element.style.opacity = "0";
-        });
-
+        refill();
     }
 
-    function isLovingOrHating(deltaX, loveCallback, hateCallback) {
-        var overTheEdge = Math.abs(deltaX) > (window.innerWidth * 0.2);
-        if (overTheEdge) {
-            if (deltaX > 0) {
-                if (loveCallback) {
-                    loveCallback();
-                }
-            } else {
-                if (hateCallback) {
-                    hateCallback();
-                }
-            }
-        }
-
-        return overTheEdge;
-    }
-
-    function loveElement() {
+    function loveElement(element) {
         love.classList.add('active');
-        deleteElement(true);
+        deleteElement(true, element);
     }
 
-    function hateElement() {
+    function hateElement(element) {
         hate.classList.add('active');
-        deleteElement(false);
-    }
-
-    function dragElement(deltaX) {
-        window.requestAnimationFrame(function () {
-            var element = currentTopImage();
-
-            love.classList.remove('active');
-            hate.classList.remove('active');
-            isLovingOrHating(deltaX, function () {
-                love.classList.add('active');
-            }, function () {
-                hate.classList.add('active');
-            });
-
-            element.style.transition = "0";
-            element.style.transform = "translate(" + deltaX + "px, " + 0 + "px) rotate(" + (deltaX / 20) + "deg)";
-            element.style.opacity = window.innerWidth / Math.abs(deltaX * 5);
-        });
-    }
-
-    function resetElement(deltaX) {
-        if (!isLovingOrHating(deltaX, loveElement, hateElement)) {
-            window.requestAnimationFrame(function () {
-                var element = currentTopImage();
-                element.style.transition = "0.05s ease-in-out";
-                element.style.transform = 'translate(0px, 0px) rotate(0deg)';
-                element.style.opacity = '1.0';
-            });
-        }
-
-    }
-
-    function initTouchListeners() {
-        halfdane.dragger(dragElement, resetElement)
-                .bind(currentTopImage());
+        deleteElement(false, element);
     }
 
     function addImageItem(item, onload) {
-        var image = document.createElement('img');
-        image.onload = onload;
-        image.src = item.imageUrl;
-        image.setAttribute('data-articleNumber', item.articleNumber);
-        image.classList.add("recommendation");
-        imagesContainer.insertBefore(image, imagesContainer.firstElementChild);
+        //<li class="pane1"><div class="img"></div><div>Miami Beach</div><div class="like"></div><div class="dislike"></div></li>
+        $imagesContainer.append(
+                $('<li>').append(
+                        $('<img>')
+                                .attr('src', item.imageUrl)
+                                .attr('data-articleNumber', item.articleNumber)
+                                .addClass('recommendation')
+                                .on('load', onload)));
     }
 
-    function loadSomeImages(afterLoad) {
-        model.getRecommendations(function (recommendations) {
-
-            if (recommendations[0]) {
-                addImageItem(recommendations[0], function () {
-                    for (var i = 1; i < recommendations.length; i++) {
-                        addImageItem(recommendations[i]);
-                    }
-                });
-            }
-
-            if (afterLoad) {
-                afterLoad();
-            }
-        });
+    function refill() {
+        if ($('.recommendation').length < 5) {
+            model.getRecommendations(function (recommendations) {
+                if (recommendations[0]) {
+                    addImageItem(recommendations[0], function () {
+                        for (var i = 1; i < recommendations.length; i++) {
+                            addImageItem(recommendations[i]);
+                        }
+                        $(".game_area").jTinder('startOver');
+                    });
+                }
+            });
+        }
     }
 
     function start() {
-        love.addEventListener('click', loveElement);
-        hate.addEventListener('click', hateElement);
-        loadSomeImages(initTouchListeners);
+        jTinder = $(".game_area").jTinder({
+            onDislike: hateElement,
+            onLike: loveElement,
+            threshold: 1
+        });
+
+        $love.on('click tap', function () {
+            $(".game_area").jTinder('like');
+        });
+
+        $hate.on('click tap', function () {
+            $(".game_area").jTinder('dislike');
+        });
+
+        refill();
     }
 
     return {
