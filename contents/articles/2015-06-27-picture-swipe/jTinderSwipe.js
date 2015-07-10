b7c0c5abc1dcad54765aa6d4211daf6197f4d7f2
halfdane <halfdane@gmx.net>
2015-07-10 13:06:09 +0200

    more simplification

--- contents/articles/2015-06-27-picture-swipe/jTinderSwipe.js
+++ contents/articles/2015-06-27-picture-swipe/jTinderSwipe.js
@@ -38,73 +38,214 @@ halfdane.fake_model = (function () {
 
     return {
         getRecommendations: getRecommendations,
-        setDecision: function () {
+        setDecision: function (articlenumber, hot) {
+            console.log(articlenumber, hot);
         }
     };
 }());
 
-halfdane.pictureswipe = function (model) {
+var jTinder = (function () {
     'use strict';
 
-    var $love = $('#love');
-    var $hate = $('#hate');
-    var $gameArea = $(".game_area");
+    var defaults = {
+        onLove: function () {
+        },
+        onHate: function () {
+        },
+        threshold: 1,
+        likeSelector: '.like',
+        dislikeSelector: '.dislike',
+        loveClickTarget: '#love',
+        hateClickTarget: '#hate'
 
-    function deleteElement(isHot, element) {
-        var articleNumber = element.attr('data-articleNumber');
-        model.setDecision(articleNumber, isHot);
+    };
 
-        refill();
+    var settings = {};
+
+    var animTime = 400;
+    var revertTime = 200;
+
+    var touchStart = false;
+    var xStart = 0;
+    var lastPosX = 0;
+    var pane_width = 0;
+    var currentPane = null;
+
+    function init(element, options) {
+        settings = $.extend({}, defaults, options);
+
+        var $element = $(element);
+
+        $element.on('touchstart', handleTouchstart);
+        $element.on('mousedown', handleMousedown);
+        $element.on('touchmove mousemove', handleMove);
+        $element.on('touchend mouseup', handleEnd);
+
+        pane_width = $element.width() * 1.5;
+
+        currentPane = function () {
+            return $(".slide", $element).last();
+        };
+
+        $(settings.loveClickTarget).on('click', love);
+        $(settings.hateClickTarget).on('click', hate);
     }
 
-    function loveElement(element) {
-        deleteElement(true, element);
+    function translateTo(pos) {
+        return {transform: "translateX(" + pos + "px) rotate(" + ((pos / pane_width) * 50) + "deg)"};
     }
 
-    function hateElement(element) {
-        deleteElement(false, element);
+    function hate() {
+        currentPane().animate(translateTo(-pane_width), animTime,
+                function () {
+                    var current = currentPane();
+                    settings.onHate(current[0]);
+                    current.remove();
+                });
+    }
+
+    function love() {
+        currentPane().animate(translateTo(pane_width), animTime,
+                function () {
+                    var current = currentPane();
+                    settings.onLove(current[0]);
+                    current.remove();
+                });
+    }
+
+    function handleTouchstart(ev) {
+        ev.preventDefault();
+        if (touchStart === false) {
+            touchStart = true;
+            xStart = ev.originalEvent.touches[0].pageX;
+        }
+    }
+
+    function handleMousedown(ev) {
+        ev.preventDefault();
+        if (touchStart === false) {
+            touchStart = true;
+            xStart = ev.pageX;
+        }
+    }
+
+    function handleMove(ev) {
+        ev.preventDefault();
+        if (touchStart === true) {
+            var pageX = typeof ev.pageX === 'undefined' ? ev.originalEvent.touches[0].pageX : ev.pageX;
+            var deltaX = parseInt(pageX) - parseInt(xStart);
+
+            currentPane().css(translateTo(deltaX));
+
+            var opa = (Math.abs(deltaX) / settings.threshold) / 100 + 0.2;
+            if (opa > 1.0) {
+                opa = 1.0;
+            }
+            if (deltaX >= 0) {
+                currentPane().find(settings.likeSelector).css('opacity', opa);
+                currentPane().find(settings.dislikeSelector).css('opacity', 0);
+            } else {
+                currentPane().find(settings.dislikeSelector).css('opacity', opa);
+                currentPane().find(settings.likeSelector).css('opacity', 0);
+            }
+        }
+    }
+
+    function handleEnd(ev) {
+        ev.preventDefault();
+        touchStart = false;
+        var pageX = (typeof ev.pageX === 'undefined') ? ev.originalEvent.changedTouches[0].pageX : ev.pageX;
+        var deltaX = parseInt(pageX) - parseInt(xStart);
+        var opa = (Math.abs(deltaX) / settings.threshold) / 100 + 0.2;
+
+        if (opa >= 1) {
+            if (deltaX > 0) {
+                love();
+            } else {
+                hate();
+            }
+        } else {
+            lastPosX = 0;
+            currentPane().animate(translateTo(0), revertTime);
+            currentPane().find(settings.likeSelector).animate({"opacity": 0}, revertTime);
+            currentPane().find(settings.dislikeSelector).animate({"opacity": 0}, revertTime);
+        }
+    }
+
+    return {
+        init: init
+    };
+
+}());
+
+halfdane.view = (function () {
+    'use strict';
+
+    function imagecontainer() {
+        return document.getElementById('images');
     }
 
     function addImageItem(item, onload) {
-        $('#images').append($('<li>')
-                .addClass('slide')
-                .append($('<img>')
-                        .attr('src', item.imageUrl)
-                        .addClass('recommendation')
-                        .attr('data-articleNumber', item.articleNumber)
-                        .on('load', onload))
-                .append($('<div>').addClass('like'))
-                .append($('<div>').addClass('dislike')));
+        var img = new Image();
+        img.onload = onload;
+        img.src = item.imageUrl;
+        img.classList.add('recommendation');
+        img.setAttribute('data-articlenumber', item.articleNumber);
+
+        var like = document.createElement('div');
+        like.classList.add('like');
+
+        var dislike = document.createElement('div');
+        dislike.classList.add('dislike');
+
+        var li = document.createElement('li');
+        li.classList.add('slide');
+        li.appendChild(img);
+        li.appendChild(like);
+        li.appendChild(dislike);
+
+        var images = imagecontainer();
+        images.insertBefore(li, images.firstChild);
+    }
+
+    function articleNumber(slide) {
+        return slide.firstChild.getAttribute('data-articlenumber');
+    }
+
+    return {
+        addImageItem: addImageItem,
+        articleNumber: articleNumber,
+        imagecontainer: imagecontainer
+    };
+
+}());
+
+halfdane.pictureswipe = function (model, view) {
+    'use strict';
+
+    function loveElement(element) {
+        model.setDecision(view.articleNumber(element), true);
+        refill();
+    }
+
+    function hateElement(element) {
+        model.setDecision(view.articleNumber(element), false);
+        refill();
     }
 
     function refill() {
-        if ($('.recommendation').length < 5) {
+        if ($('.recommendation').length <= 5) {
             model.getRecommendations(function (recommendations) {
-                if (recommendations[0]) {
-                    addImageItem(recommendations[0], function () {
-                        for (var i = 1; i < recommendations.length; i++) {
-                            addImageItem(recommendations[i]);
-                        }
-                        jTinder.startOver($gameArea);
-                    });
-                }
+                recommendations.forEach(view.addImageItem);
             });
         }
     }
 
     function start() {
-        jTinder.init($gameArea, {
-            onDislike: hateElement,
-            onLike: loveElement,
+        jTinder.init(view.imagecontainer(), {
+            onLove: hateElement,
+            onHate: loveElement,
             threshold: 2
-        });
-
-        $love.on('click', function () {
-            jTinder.love();
-        });
-
-        $hate.on('click', function () {
-            jTinder.hate();
         });
 
         refill();
@@ -118,5 +259,5 @@ halfdane.pictureswipe = function (model) {
 document.addEventListener('DOMContentLoaded', function () {
     'use strict';
 
-    halfdane.pictureswipe(halfdane.fake_model).start();
+    halfdane.pictureswipe(halfdane.fake_model, halfdane.view).start();
 });
--- contents/articles/2015-06-27-picture-swipe/picture-swipe2.md
+++ contents/articles/2015-06-27-picture-swipe/picture-swipe2.md
@@ -2,9 +2,7 @@
 template: empty.jade
 ---
 <link rel="stylesheet" type="text/css" href="jTinder.css">
-<div class="game_area">
-    <ul id="images" class="container"></ul>
-</div>
+<ul id="images" class="container"></ul>
 <a href="#" class="action hate" id="hate" ><i></i></a>
 <a href="#" class="action love" id="love" ><i></i></a>
 
@@ -13,9 +11,5 @@ template: empty.jade
 
 <!-- transform2d lib -->
 <script type="text/javascript" src="jquery.transform2d.js"></script>
-
-<!-- jTinder lib -->
-<script type="text/javascript" src="jquery.jTinder.js"></script>
-
 
 <script type="text/javascript" src="jTinderSwipe.js"></script>
