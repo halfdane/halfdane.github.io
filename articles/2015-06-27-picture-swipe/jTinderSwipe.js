9f7ee3ac5894092b1a81d49c2c33fbcd3391df7c
halfdane <halfdane@gmx.net>
2015-07-10 12:06:40 +0000

    [ci skip] snapshot 07/10/15 12:06

--- articles/2015-06-27-picture-swipe/jTinderSwipe.js
+++ articles/2015-06-27-picture-swipe/jTinderSwipe.js
@@ -44,140 +44,6 @@ halfdane.fake_model = (function () {
     };
 }());
 
-var jTinder = (function () {
-    'use strict';
-
-    var defaults = {
-        onLove: function () {
-        },
-        onHate: function () {
-        },
-        threshold: 1,
-        likeSelector: '.like',
-        dislikeSelector: '.dislike',
-        loveClickTarget: '#love',
-        hateClickTarget: '#hate'
-
-    };
-
-    var settings = {};
-
-    var animTime = 400;
-    var revertTime = 200;
-
-    var touchStart = false;
-    var xStart = 0;
-    var lastPosX = 0;
-    var pane_width = 0;
-    var currentPane = null;
-
-    function init(element, options) {
-        settings = $.extend({}, defaults, options);
-
-        var $element = $(element);
-
-        $element.on('touchstart', handleTouchstart);
-        $element.on('mousedown', handleMousedown);
-        $element.on('touchmove mousemove', handleMove);
-        $element.on('touchend mouseup', handleEnd);
-
-        pane_width = $element.width() * 1.5;
-
-        currentPane = function () {
-            return $(".slide", $element).last();
-        };
-
-        $(settings.loveClickTarget).on('click', love);
-        $(settings.hateClickTarget).on('click', hate);
-    }
-
-    function translateTo(pos) {
-        return {transform: "translateX(" + pos + "px) rotate(" + ((pos / pane_width) * 50) + "deg)"};
-    }
-
-    function hate() {
-        currentPane().animate(translateTo(-pane_width), animTime,
-                function () {
-                    var current = currentPane();
-                    settings.onHate(current[0]);
-                    current.remove();
-                });
-    }
-
-    function love() {
-        currentPane().animate(translateTo(pane_width), animTime,
-                function () {
-                    var current = currentPane();
-                    settings.onLove(current[0]);
-                    current.remove();
-                });
-    }
-
-    function handleTouchstart(ev) {
-        ev.preventDefault();
-        if (touchStart === false) {
-            touchStart = true;
-            xStart = ev.originalEvent.touches[0].pageX;
-        }
-    }
-
-    function handleMousedown(ev) {
-        ev.preventDefault();
-        if (touchStart === false) {
-            touchStart = true;
-            xStart = ev.pageX;
-        }
-    }
-
-    function handleMove(ev) {
-        ev.preventDefault();
-        if (touchStart === true) {
-            var pageX = typeof ev.pageX === 'undefined' ? ev.originalEvent.touches[0].pageX : ev.pageX;
-            var deltaX = parseInt(pageX) - parseInt(xStart);
-
-            currentPane().css(translateTo(deltaX));
-
-            var opa = (Math.abs(deltaX) / settings.threshold) / 100 + 0.2;
-            if (opa > 1.0) {
-                opa = 1.0;
-            }
-            if (deltaX >= 0) {
-                currentPane().find(settings.likeSelector).css('opacity', opa);
-                currentPane().find(settings.dislikeSelector).css('opacity', 0);
-            } else {
-                currentPane().find(settings.dislikeSelector).css('opacity', opa);
-                currentPane().find(settings.likeSelector).css('opacity', 0);
-            }
-        }
-    }
-
-    function handleEnd(ev) {
-        ev.preventDefault();
-        touchStart = false;
-        var pageX = (typeof ev.pageX === 'undefined') ? ev.originalEvent.changedTouches[0].pageX : ev.pageX;
-        var deltaX = parseInt(pageX) - parseInt(xStart);
-        var opa = (Math.abs(deltaX) / settings.threshold) / 100 + 0.2;
-
-        if (opa >= 1) {
-            if (deltaX > 0) {
-                love();
-            } else {
-                hate();
-            }
-        } else {
-            lastPosX = 0;
-            currentPane().animate(translateTo(0), revertTime);
-            currentPane().find(settings.likeSelector).animate({"opacity": 0}, revertTime);
-            currentPane().find(settings.dislikeSelector).animate({"opacity": 0}, revertTime);
-        }
-    }
-
-    return {
-        init: init
-    };
-
-}());
-
 halfdane.view = (function () {
     'use strict';
 
@@ -212,16 +78,109 @@ halfdane.view = (function () {
         return slide.firstChild.getAttribute('data-articlenumber');
     }
 
+    function lastPane() {
+        return imagecontainer().querySelector('.slide:last-child');
+    }
+
     return {
         addImageItem: addImageItem,
         articleNumber: articleNumber,
-        imagecontainer: imagecontainer
+        imagecontainer: imagecontainer,
+        lastPane: lastPane
     };
 
 }());
 
 halfdane.pictureswipe = function (model, view) {
     'use strict';
+
+    var animTime = 400;
+    var revertTime = 200;
+
+    var touchStart = false;
+    var xStart = 0;
+    var lastPosX = 0;
+    var pane_width = 0;
+    var threshold = 2;
+    var likeSelector = '.like',
+            dislikeSelector = '.dislike',
+            loveClickTarget = '#love',
+            hateClickTarget = '#hate';
+
+    function translateTo(pos) {
+        return {transform: "translateX(" + pos + "px) rotate(" + ((pos / pane_width) * 50) + "deg)"};
+    }
+
+    function hate() {
+        var current = $(view.lastPane());
+        current.animate(translateTo(-pane_width), animTime,
+                function () {
+                    hateElement(current[0]);
+                    current.remove();
+                });
+    }
+
+    function love() {
+        var current = $(view.lastPane());
+        current.animate(translateTo(pane_width), animTime,
+                function () {
+                    loveElement(current[0]);
+                    current.remove();
+                });
+    }
+
+    function handleStart(ev) {
+        ev.preventDefault();
+        if (touchStart === false) {
+            touchStart = true;
+            xStart = ev.pageX || ev.touches[0].pageX;
+        }
+    }
+
+    function handleMove(ev) {
+        ev.preventDefault();
+        if (touchStart === true) {
+            var pageX = ev.pageX || ev.touches[0].pageX;
+            var deltaX = parseInt(pageX) - parseInt(xStart);
+            var opa = (Math.abs(deltaX) / threshold) / 100 + 0.2;
+            var currentPane = $(view.lastPane());
+
+            currentPane.css(translateTo(deltaX));
+
+            if (opa > 1.0) {
+                opa = 1.0;
+            }
+            if (deltaX >= 0) {
+                currentPane.find(likeSelector).css('opacity', opa);
+                currentPane.find(dislikeSelector).css('opacity', 0);
+            } else {
+                currentPane.find(dislikeSelector).css('opacity', opa);
+                currentPane.find(likeSelector).css('opacity', 0);
+            }
+        }
+    }
+
+    function handleEnd(ev) {
+        ev.preventDefault();
+        touchStart = false;
+        var pageX = ev.pageX || ev.changedTouches[0].pageX;
+        var deltaX = parseInt(pageX) - parseInt(xStart);
+        var opa = (Math.abs(deltaX) / threshold) / 100 + 0.2;
+        var currentPane = $(view.lastPane());
+
+        if (opa >= 1) {
+            if (deltaX > 0) {
+                love();
+            } else {
+                hate();
+            }
+        } else {
+            lastPosX = 0;
+            currentPane.animate(translateTo(0), revertTime);
+            currentPane.find(likeSelector).animate({"opacity": 0}, revertTime);
+            currentPane.find(dislikeSelector).animate({"opacity": 0}, revertTime);
+        }
+    }
 
     function loveElement(element) {
         model.setDecision(view.articleNumber(element), true);
@@ -242,12 +201,19 @@ halfdane.pictureswipe = function (model, view) {
     }
 
     function start() {
-        jTinder.init(view.imagecontainer(), {
-            onLove: hateElement,
-            onHate: loveElement,
-            threshold: 2
-        });
+        var $element = $(view.imagecontainer());
 
+        view.imagecontainer().addEventListener('touchstart', handleStart);
+        view.imagecontainer().addEventListener('mousedown', handleStart);
+        view.imagecontainer().addEventListener('touchmove', handleMove);
+        view.imagecontainer().addEventListener('mousemove', handleMove);
+        view.imagecontainer().addEventListener('touchend', handleEnd);
+        view.imagecontainer().addEventListener('mouseup', handleEnd);
+
+        $(loveClickTarget).on('click', love);
+        $(hateClickTarget).on('click', hate);
+
+        pane_width = $element.width() * 1.5;
         refill();
     }
 
