9d0cb0788095c553c01328763fe424c7b6481850
halfdane <halfdane@gmx.net>
2016-10-11 18:52:07 +0000

    [ci skip] snapshot 10/11/16 18:52

--- dh/resources/js/exponential.js
+++ dh/resources/js/exponential.js
@@ -2,173 +2,241 @@
 
 window.exponential = window.exponential || {};
 
-window.exponential.graph = function (canvas) {
+window.exponential.graph = (function () {
 
-    var ctx = canvas.getContext('2d');
-    var iteration;
+    var canvas;
+    var ctx;
 
-    var scaleX;
-    var scaleY;
+    var scaled = (function () {
+        var iteration;
 
-    var animateHighlight;
-    var shouldDrawHighlight;
-    var currentHighlight;
-    var highlightEnd;
+        var scaleX;
+        var scaleY;
 
-    var drawFct;
-    var equationFct;
+        var maxX = 5, maxY = 100000;
 
-    var maxX = 5, maxY = 100000;
-    var targetMaxX;
-    var targetMaxY;
-    var xStep, yStep;
+        var targetMaxX;
+        var targetMaxY;
+        var xStep, yStep;
 
-    function scaleTo(maxX, maxY) {
-        iteration = maxX / 1000;
+        function draw(callback) {
+            ctx.save();
+            ctx.translate(0, canvas.height);
+            ctx.scale(scaleX, -scaleY);
+            ctx.beginPath();
 
-        scaleX = canvas.width / maxX;
-        scaleY = canvas.height / maxY;
-    }
+            callback(iteration, maxX, scaleX, scaleY);
 
-    function useEquation(equationFunction) {
-        return function () {
-            equationFct = equationFunction;
-        };
-    }
+            ctx.restore();
 
-    function zoomTo(newMaxX, newMaxY) {
-        return function () {
-            animateHighlight = false;
-            shouldDrawHighlight = false;
+            ctx.lineJoin = 'round';
+            ctx.lineWidth = 1;
+            ctx.stroke();
+        }
 
+        function zoomTo(newMaxX, newMaxY) {
             targetMaxX = newMaxX;
             targetMaxY = newMaxY;
 
             xStep = (targetMaxX - maxX) / 100;
             yStep = (targetMaxY - maxY) / 100;
 
-            scaleTo(maxX, maxY);
-        };
-    }
+            zoomStep(maxX, maxY);
+        }
 
-    function activateHighlight(highlightStart) {
-        return function () {
+        function set(maxX, maxY) {
+            iteration = maxX / 1000;
+
+            scaleX = canvas.width / maxX;
+            scaleY = canvas.height / maxY;
+        }
+
+        function zoomStep() {
+            var xDiff = Math.abs(targetMaxX - maxX);
+            var yDiff = Math.abs(targetMaxY - maxY);
+
+            if (xDiff > 0.1 || yDiff > 0.1) {
+                if (xDiff > 0.1) {
+                    maxX += xStep;
+                }
+                if (yDiff > 0.1) {
+                    maxY += yStep;
+                }
+                set(maxX, maxY);
+            }
+        }
+
+        return {
+            set: set,
+            draw: draw,
+            zoomTo: zoomTo,
+            zoomStep: zoomStep
+        };
+    })();
+
+    var axes = (function () {
+        function draw() {
+            ctx.save();
+            ctx.translate(0, canvas.height);
+            ctx.scale(1, -1);
+
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(canvas.width, 0);
+            ctx.strokeStyle = '#aaa';
+            ctx.lineWidth = 2;
+            ctx.stroke();
+
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(0, canvas.height);
+            ctx.strokeStyle = '#aaa';
+            ctx.lineWidth = 2;
+            ctx.stroke();
+
+            ctx.restore();
+        }
+
+        return {
+            draw: draw
+        }
+    })();
+
+    var equation = (function () {
+        var equationFunction;
+
+        function set(newEquationFunction) {
+            equationFunction = newEquationFunction;
+        }
+
+        function calculate(x) {
+            return equationFunction(x);
+        }
+
+        function draw() {
+            scaled.draw(function (iteration, maxX, scaleX, scaleY) {
+                for (var x = 0 + iteration; x <= maxX; x += iteration) {
+                    ctx.fillStyle = 'red';
+                    ctx.fillRect(x, equationFunction(x), 2 / scaleX, 2 / scaleY);
+                }
+            });
+        }
+
+        return {
+            set: set,
+            draw: draw,
+            calculate: calculate
+        }
+    })(scaled);
+
+    var highLight = (function (equation) {
+        var animateHighlight;
+        var shouldDrawHighlight;
+        var currentHighlight;
+        var highlightEnd;
+
+        function reset() {
+            animateHighlight = false;
+            shouldDrawHighlight = false;
+        }
+
+        function activate(highlightStart) {
             shouldDrawHighlight = true;
             currentHighlight = highlightStart;
-        };
-    }
+        }
 
-    function activateHighlightAnimation(to) {
-        return function () {
+        function animateTo(to) {
             animateHighlight = true;
             highlightEnd = to;
-        };
-    }
-
-    function drawAxes() {
-        ctx.save();
-        ctx.translate(0, canvas.height);
-        ctx.scale(1, -1);
-
-        ctx.beginPath();
-        ctx.moveTo(0, 0);
-        ctx.lineTo(canvas.width, 0);
-        ctx.strokeStyle = '#aaa';
-        ctx.lineWidth = 2;
-        ctx.stroke();
-
-        ctx.beginPath();
-        ctx.moveTo(0, 0);
-        ctx.lineTo(0, canvas.height);
-        ctx.strokeStyle = '#aaa';
-        ctx.lineWidth = 2;
-        ctx.stroke();
-
-        ctx.restore();
-    }
-
-    function inGraph(callback) {
-        ctx.save();
-        ctx.translate(0, canvas.height);
-        ctx.scale(scaleX, -scaleY);
-        ctx.beginPath();
-
-        callback();
-
-        ctx.restore();
-
-        ctx.lineJoin = 'round';
-        ctx.lineWidth = 1;
-        ctx.stroke();
-    }
-
-    function zoomScale() {
-        var xDiff = Math.abs(targetMaxX - maxX);
-        var yDiff = Math.abs(targetMaxY - maxY);
-
-        if (xDiff > 0.1 || yDiff > 0.1) {
-            if (xDiff > 0.1) {
-                maxX += xStep;
-            }
-            if (yDiff > 0.1) {
-                maxY += yStep;
-            }
-
-            scaleTo(maxX, maxY);
-        }
-    }
-
-    function drawHighlight(equation) {
-        if (currentHighlight > highlightEnd) {
-            if (animateHighlight) {
-                currentHighlight -= iteration;
-            }
         }
 
-        inGraph(function () {
-            ctx.moveTo(currentHighlight, 0);
-            ctx.lineTo(currentHighlight, equation(currentHighlight));
-
-            ctx.moveTo(0, equation(currentHighlight));
-            ctx.lineTo(currentHighlight, equation(currentHighlight));
-            ctx.strokeStyle = 'blue';
-        });
-    }
-
-    function draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-        drawAxes();
-
-        inGraph(function () {
-            for (var x = 0 + iteration; x <= maxX; x += iteration) {
-                ctx.fillStyle = 'red';
-                ctx.fillRect(x, equationFct(x), 2 / scaleX, 2 / scaleY);
+        function draw() {
+            if (!shouldDrawHighlight) {
+                return;
             }
-        });
 
-        if (shouldDrawHighlight) {
-            drawHighlight(equationFct);
+            scaled.draw(function (iteration) {
+                if (currentHighlight > highlightEnd) {
+                    if (animateHighlight) {
+                        currentHighlight -= iteration;
+                    }
+                }
+
+                ctx.moveTo(currentHighlight, 0);
+                ctx.lineTo(currentHighlight, equation.calculate(currentHighlight));
+
+                ctx.moveTo(0, equation.calculate(currentHighlight));
+                ctx.lineTo(currentHighlight, equation.calculate(currentHighlight));
+                ctx.strokeStyle = 'blue';
+            });
         }
 
-        zoomScale();
-    }
+        return {
+            reset: reset,
+            activate: activate,
+            animateTo: animateTo,
+            draw: draw
+        }
+    })(equation);
 
-    return {
-        draw: draw,
-        useEquation: useEquation,
-        activateHighlight: activateHighlight,
-        activateHighlightAnimation: activateHighlightAnimation,
-        zoomTo: zoomTo
-    }
-};
+    var steps = (function(){
+        var running = false;
+        var steps = [
+            function () {
+                highLight.activate(4);
+            },
+            function () {
+                highLight.animateTo(3)
+            },
+            function () {
+                highLight.reset();
+                scaled.zoomTo(100, 100);
+            },
+            function () {
+                equation.set(function (x) {
+                    return Math.pow(17, x) % 97;
+                });
+            },
+            function () {
+                highLight.activate(50);
+            },
+            function () {
+                highLight.animateTo(30)
+            }
+        ];
+        var currentStep = 0;
 
-window.exponential.simple = (function () {
+        function init() {
+            running = true;
+            currentStep = 0;
+        }
 
-    var running = false;
-    var graph;
-    var steps;
-    var currentStep;
+        function stop() {
+            running = false;
+        }
+
+        function hasMoreSteps() {
+            return running && currentStep < steps.length;
+        }
+
+        function step() {
+            steps[currentStep++]();
+        }
+
+        function isRunning() {
+            return running;
+        }
+
+        return {
+            init: init,
+            stop: stop,
+            hasMoreSteps: hasMoreSteps,
+            isRunning: isRunning,
+            step: step
+        }
+    })();
+
+
 
     function init(current) {
         var $canvas = current.find('canvas')
@@ -176,55 +244,40 @@ window.exponential.simple = (function () {
                 .prop({width: 500, height: 500});
         $canvas.trigger('resize.deckscale');
 
-        graph = exponential.graph($canvas[0]);
-        graph.useEquation(function (x) {
-            return Math.pow(17, x);
-        })();
-        graph.zoomTo(5, 100000)();
+        canvas = $canvas[0];
+        ctx = canvas.getContext('2d');
+        steps.init();
 
-        steps = [
-            graph.activateHighlight(4),
-            graph.activateHighlightAnimation(3),
-            graph.zoomTo(100, 100),
-            graph.useEquation(function (x) {
-                return Math.pow(17, x) % 97;
-            }),
-            graph.activateHighlight(50),
-            graph.activateHighlightAnimation(30)
-        ];
-        running = true;
-        currentStep = 0;
+        equation.set(function (x) {
+            return Math.pow(17, x);
+        });
+        highLight.reset();
+        scaled.set(5, 100000);
+        scaled.zoomTo(5, 100000);
+
 
         window.requestAnimationFrame(draw);
     }
 
-    function stop() {
-        running = false;
-    }
-
-    function hasMoreSteps() {
-        return currentStep < steps.length;
-    }
-
-    function step() {
-        steps[currentStep++]();
-    }
-
-    function draw(timestamp) {
-        if (!running) {
+    function draw() {
+        if (!steps.isRunning()) {
             return;
         }
 
-        graph.draw(timestamp);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        axes.draw();
+        equation.draw();
+        highLight.draw();
+        scaled.zoomStep();
 
         window.requestAnimationFrame(draw);
     }
 
     return {
         init: init,
-        hasMoreSteps: hasMoreSteps,
-        step: step,
-        stop: stop
+        hasMoreSteps: steps.hasMoreSteps,
+        step: steps.step,
+        stop: steps.stop
     };
 })();
   
