37423732a52168cd779fe3d78a18dd5a1841927f
halfdane <halfdane@gmx.net>
2016-10-16 18:03:35 +0200

    Intro is done

--- contents/dh/resources/js/intro.js
+++ contents/dh/resources/js/intro.js
@@ -12,15 +12,37 @@ window.dh.intro = (function () {
     var halfHeight = maxHeight / 2;
     var halfActorWidth = actorWidth / 2;
 
-    function activatable(something) {
+    function makeActivatable(something) {
+        var active = false;
+        var originalDraw = something.draw;
 
+        something.draw = function (progress) {
+            if (!!originalDraw && active) {
+                originalDraw(progress);
+            }
+        };
+
+        something.activate = function () {
+            active = true;
+        };
+
+        something.deactivate = function () {
+            active = false;
+        };
+
+        return something;
     }
 
     var createImages = function (onComplete) {
         var images = [
             'resources/img/sean_connery.jpg',
             'resources/img/m.jpg',
-            'resources/img/dr_no.jpg'];
+            'resources/img/dr_no.jpg',
+            'resources/img/client.jpg',
+            'resources/img/ebay.png',
+            'resources/img/hacker.jpg',
+            'resources/img/otpbooklet.jpg',
+        ];
         var imageObjects = [];
 
         var loaded = 0;
@@ -28,7 +50,15 @@ window.dh.intro = (function () {
         function onLoad() {
             loaded++;
             if (loaded === images.length) {
-                onComplete();
+                onComplete({
+                    jb: imageObjects[0],
+                    m: imageObjects[1],
+                    no: imageObjects[2],
+                    client: imageObjects[3],
+                    shop: imageObjects[4],
+                    hacker: imageObjects[5],
+                    codebook: imageObjects[6],
+                });
             }
         }
 
@@ -38,78 +68,67 @@ window.dh.intro = (function () {
             img.src = images[i];
             imageObjects.push(img);
         }
-
-        return {
-            jb: imageObjects[0],
-            m: imageObjects[1],
-            no: imageObjects[2]
-        }
     };
 
-    var createActor = function (ctx, x, y, image) {
+    var createActor = function (ctx, x, y, image, alternateImage) {
         var width = 100,
-                height = image.height * (width / image.width),
-                active = false;
+                currentImage = image;
 
         function draw() {
-            if (active) {
-                ctx.drawImage(image, x, y, width, height);
-            }
+            var height = currentImage.height * (width / currentImage.width)
+            ctx.drawImage(currentImage, x, y, width, height);
         }
 
-        function activate() {
-            console.log('Activating', image.src);
-            active = true;
-        }
-
-        function deactivate() {
-            active = false;
-        }
-
-        return {
+        return makeActivatable({
             draw: draw,
-            activate: activate,
-            deactivate: deactivate
-        };
+            useJB: function () {
+                currentImage = alternateImage;
+            },
+            useNormal: function () {
+                currentImage = image;
+            }
+        });
     };
 
-    var createMessage = function (ctx, x, y) {
-        var active = false,
-                targetX = x,
-                step = 1,
-                moveFurther = function () {
-                    return false;
-                };
+    var createMessage = function (ctx, x, y, str) {
+        var targetX , targetY, xStep, yStep,
+                moveX, moveY,
+                xOffset, yOffset,
+                message;
 
         function draw(progress) {
-            if (active) {
-                if (moveFurther(x)) {
-                    x += progress * step;
-                }
-
-                ctx.font = "48px serif";
-                ctx.fillText("7626", x, y);
+            if (moveX(x + xOffset)) {
+                xOffset += progress * xStep;
             }
-        }
+            if (moveY(y + yOffset)) {
+                yOffset += progress * yStep;
+            }
 
-        function activate() {
-            console.log('Activating message');
-            active = true;
-        }
-
-        function deactivate() {
-            active = false;
+            ctx.font = "48px serif";
+            ctx.fillText(message, x + xOffset, y + yOffset);
         }
 
         function calcStep() {
-            step = (targetX - x) / 500;
-            if (targetX > x) {
-                moveFurther = function (x) {
-                    return x < targetX
+            console.log('Adjusting', str);
+            xStep = (targetX - x + xOffset) / maxWidth;
+            if (targetX > x + xOffset) {
+                moveX = function (x) {
+                    return x < targetX;
                 };
             } else {
-                moveFurther = function (x) {
-                    return x > targetX
+                moveX = function (x) {
+                    return x > targetX;
+                };
+            }
+
+            yStep = (targetY - y + yOffset) / maxWidth;
+            if (targetY > y + yOffset) {
+                moveY = function (y) {
+                    return y < targetY;
+                };
+            } else {
+                moveY = function (y) {
+                    return y > targetY;
                 };
             }
         }
@@ -120,45 +139,81 @@ window.dh.intro = (function () {
         }
 
         function moveToFull() {
-            targetX = maxWidth - actorWidth-10;
+            targetX = maxWidth - actorWidth - 40;
             calcStep();
         }
 
-        return {
+        function moveDown() {
+            targetY = 440;
+            calcStep();
+        }
+
+        function setStart(newX, newY) {
+            x = newX || x;
+            y = newY || y;
+            targetX = newX || x;
+            targetY = newY || y;
+            xStep = 1;
+            yStep = 1;
+            moveX = function (x) {
+                return false;
+            };
+            moveY = function (y) {
+                return false;
+            };
+            xOffset = 0;
+            yOffset = 0;
+            message = '7626';
+            calcStep();
+        }
+
+        function setMessage(newMessage) {
+            message = newMessage;
+        }
+
+        setStart();
+
+        return makeActivatable({
             draw: draw,
-            activate: activate,
-            deactivate: deactivate,
             moveToHalf: moveToHalf,
-            moveToFull: moveToFull
-        };
+            moveToFull: moveToFull,
+            moveDown: moveDown,
+            setStart: setStart,
+            setMessage: setMessage
+        });
     };
 
-    var createProtocol = function(ctx, y) {
-        var active = false;
-
+    var createProtocol = function (ctx, y) {
         function draw() {
-            if (active) {
-                ctx.beginPath();
-                ctx.moveTo(actorWidth, y+5);
-                ctx.lineTo(maxWidth - actorWidth, y+5);
-                ctx.stroke();
-            }
+            ctx.beginPath();
+            ctx.moveTo(actorWidth, y);
+            ctx.lineTo(maxWidth - actorWidth, y);
+            ctx.stroke();
         }
 
-        function activate() {
-            console.log('Activating protocol');
-            active = true;
+        return makeActivatable({draw: draw});
+    };
+
+    var createProtocolDots = function (ctx, y) {
+        function draw() {
+            ctx.beginPath();
+            ctx.arc(halfWidth, y, 2, 0, 2 * Math.PI);
+            ctx.fill();
+            ctx.stroke();
         }
 
-        function deactivate() {
-            active = false;
+        return makeActivatable({draw: draw});
+    };
+
+    var createProtocolListener = function (ctx, y) {
+        function draw() {
+            ctx.beginPath();
+            ctx.moveTo(halfWidth, y);
+            ctx.lineTo(halfWidth, y + 100);
+            ctx.stroke();
         }
 
-        return {
-            draw: draw,
-            activate: activate,
-            deactivate: deactivate
-        };
+        return makeActivatable({draw: draw});
     };
 
     var steps = (function () {
@@ -168,18 +223,88 @@ window.dh.intro = (function () {
         var currentStep = 0;
 
         function init(client, server, intruder,
-                      message, protocol) {
+                      message, messageCopy, protocol,
+                      protocolDots, protocolListener,
+                      codebook1, codebook2) {
             running = true;
             currentStep = 0;
 
             steps = [
-                message.activate,
                 server.activate,
+                message.activate,
                 protocol.activate,
                 message.moveToHalf,
-                message.moveToFull,
-                message.moveToHalf,
+                protocolDots.activate,
+                protocolListener.activate,
                 intruder.activate,
+                function () {
+                    message.moveToFull();
+                    messageCopy.activate();
+                    messageCopy.moveDown();
+                },
+                function () {
+                    message.deactivate();
+                    message.setStart();
+                    protocol.deactivate();
+                    messageCopy.deactivate();
+                    messageCopy.setStart();
+                    protocolDots.deactivate();
+                    protocolListener.deactivate();
+                    client.useJB();
+                    server.deactivate();
+                    server.useJB();
+                    intruder.deactivate();
+                    intruder.useJB();
+                },
+                function () {
+                    message.activate();
+                    server.activate();
+                    protocol.activate();
+                },
+                function () {
+                    protocolDots.activate();
+                    protocolListener.activate();
+                    intruder.activate();
+                },
+                function () {
+                    codebook1.activate();
+                    codebook2.activate();
+                },
+                function () {
+                    message.setMessage('7626 + 60811');
+                },
+                function () {
+                    message.setMessage(7626 + 60811);
+                    messageCopy.setMessage(7626 + 60811);
+                },
+                message.moveToHalf,
+                function () {
+                    message.moveToFull();
+                    messageCopy.activate();
+                    messageCopy.moveDown();
+                },
+                function () {
+                    message.setStart(200);
+                    message.setMessage((7626 + 60811) + '- 60811');
+                },
+                function () {
+                    message.setStart(maxWidth - actorWidth - 40);
+                    message.setMessage('7626');
+                },
+                function () {
+                    message.setStart(1);
+                    messageCopy.deactivate();
+                    client.useNormal();
+                    server.useNormal();
+                    intruder.useNormal();
+                },
+                function () {
+                    codebook1.deactivate();
+                    codebook2.deactivate();
+                },
+                function () {
+                    message.setMessage('7626 + ????');
+                }
             ];
         }
 
@@ -211,45 +336,58 @@ window.dh.intro = (function () {
     function init(canvas) {
         var ctx = canvas.getContext('2d');
 
-        var images = createImages(function () {
+        createImages(function (images) {
+            var client = createActor(ctx, 0, 200, images.client, images.jb);
+            client.activate();
+            var server = createActor(ctx, maxWidth - actorWidth, 200, images.shop, images.m);
+            var intruder = createActor(ctx, halfWidth - halfActorWidth, 300, images.hacker, images.no);
+            var codebook1 = createActor(ctx, 0, 40, images.codebook);
+            var codebook2 = createActor(ctx, maxWidth - actorWidth, 40, images.codebook);
+            var message = createMessage(ctx, 0, 180, 'orig');
+
+            var messageCopy = createMessage(ctx, halfWidth - halfActorWidth, 180, 'copy');
+
+            var protocol = createProtocol(ctx, 185);
+            var protocolDots = createProtocolDots(ctx, 185);
+            var protocolListener = createProtocolListener(ctx, 185);
+
+            steps.init(client, server, intruder, message, messageCopy, protocol,
+                    protocolDots, protocolListener,
+                    codebook1, codebook2);
+
             window.requestAnimationFrame(draw);
-        });
 
-        var client = createActor(ctx, 0, 200, images.jb);
-        client.activate();
-        var server = createActor(ctx, maxWidth - actorWidth, 200, images.m);
-        var intruder = createActor(ctx, halfWidth - halfActorWidth, 300, images.no);
-        var message = createMessage(ctx, 0, 180);
+            var lastTime;
+            var progress;
 
-        var protocol = createProtocol(ctx, 180);
+            function draw(currentTime) {
+                if (!steps.isRunning()) {
+                    return;
+                }
 
-        steps.init(client, server, intruder, message, protocol);
+                if (!lastTime) {
+                    lastTime = currentTime;
+                }
 
-        var lastTime;
-        var progress;
-
-        function draw(currentTime) {
-            if (!steps.isRunning()) {
-                return;
-            }
-
-            if (!lastTime) {
+                progress = (currentTime - lastTime);
                 lastTime = currentTime;
+
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+                client.draw(progress);
+                server.draw(progress);
+                intruder.draw(progress);
+                message.draw(progress);
+                messageCopy.draw(progress);
+                protocol.draw(progress);
+                protocolDots.draw(progress);
+                protocolListener.draw(progress);
+                codebook1.draw(progress);
+                codebook2.draw(progress);
+
+                window.requestAnimationFrame(draw);
             }
-
-            progress = (currentTime - lastTime);
-            lastTime = currentTime;
-
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-            client.draw(progress);
-            server.draw(progress);
-            intruder.draw(progress);
-            message.draw(progress);
-            protocol.draw(progress);
-
-            window.requestAnimationFrame(draw);
-        }
+        });
 
     }
 
