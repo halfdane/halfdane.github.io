8e4a648f5f7f002b63a7b5d83867c1c57507a9d9
halfdane <halfdane@gmx.net>
2016-11-24 19:24:30 +0100

    proper graphs for elliptical curves.

--- contents/dh/resources/js/elliptic2.js
+++ contents/dh/resources/js/elliptic2.js
@@ -4,13 +4,15 @@ window.dh = window.dh || {};
 
 window.dh.elliptic2 = (function () {
 
-    var a = -3;
-    var b = 3;
+    var a;
+    var b;
+
+    var strokewidth= 3;
 
     var stepper = createStepper();
 
     function f(x) {
-        return Math.sqrt(x * x * x + a * x + b);
+        return Math.sqrt(x * x * x + a.Value() * x + b.Value());
     }
 
     function inverseF(x) {
@@ -19,29 +21,60 @@ window.dh.elliptic2 = (function () {
 
     function init(target) {
         var board = JXG.JSXGraph.initBoard(target.id, {
-            boundingbox: [-10, 10, 10, -10],
+            boundingbox: [-5, 5, 5, -5],
             axis: true,
             showCopyright: false
         });
 
-        var graph = board.create('functiongraph', [f]);
-        var graph2 = board.create('functiongraph', [inverseF]);
+        a = board.create('slider', [[0,-3],[4,-3],[-5,-3,5]]);
+        b = board.create('slider', [[0,-4],[4,-4],[-5,3,5]]);
 
-        var tangent;
+        var graph1 = board.create('functiongraph', [f], {strokeWidth: strokewidth});
+        var graph2 = board.create('functiongraph', [inverseF], {strokeWidth: strokewidth});
 
+        var originalPoint, point,
+                tangent,
+                intersect;
 
-        var steps = [
+        stepper.use([
             function () {
-                var point = board.create('glider', [graph]);
-                tangent = board.create('tangent', [point], {strokeColor: '#ff0000', dash: 2});
+                originalPoint = point = board.create('glider', [graph1]);
             },
             function () {
-                board.create('intersection', [graph, tangent, 0]);
-                board.create('intersection', [graph2, tangent, 0]);
+                tangent = board.create('tangent', [point], {strokeColor: '#ff0000', dash: 2, strokeWidth: strokewidth});
+            },
+            function () {
+                intersect = board.create('intersection', [graph1, tangent, 0]);
+            },
+            function () {
+                point = board.create('glider', [function() {return intersect.X()}, function () {return -intersect.Y()}, graph2]);
+                board.create('line', [intersect, point], {dash: 3, straightFirst: false, straightLast:false, strokeWidth: strokewidth, lastArrow: true});
+            },
+            function () {
+                var target = -1.26;
+                originalPoint.moveTo([target, f(target)], 2000);
+            },
+            function() {
+                tangent = board.create('tangent', [point], {strokeColor: '#ff0000', dash: 2, strokeWidth: strokewidth});
+                intersect = board.create('intersection', [graph1, tangent, 0]);
+            },
+            function () {
+                point = board.create('glider', [function() {return intersect.X()}, function () {return -intersect.Y()}, graph2]);
+                board.create('line', [intersect, point], {dash: 3, straightFirst: false, straightLast:false, strokeWidth: strokewidth, lastArrow: true});
+            },
+            function() {
+                tangent = board.create('tangent', [point], {strokeColor: '#ff0000', dash: 2, strokeWidth: strokewidth});
+                intersect = board.create('intersection', [graph2, tangent, 0]);
+            },
+            function () {
+                point = board.create('glider', [function() {return intersect.X()}, function () {return -intersect.Y()}, graph1]);
+                board.create('line', [intersect, point], {dash: 3, straightFirst: false, straightLast:false, strokeWidth: strokewidth, lastArrow: true});
+            },
+            function() {
+                tangent = board.create('tangent', [point], {strokeColor: '#ff0000', dash: 2, strokeWidth: strokewidth});
+                intersect = board.create('intersection', [graph1, tangent, 0]);
             }
-        ];
-
-        stepper.use(steps);
+        ]);
     }
 
     return {
